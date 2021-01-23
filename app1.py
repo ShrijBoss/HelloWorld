95091470997f00098cab0c71deb1657477e6e603
shrijayshastri <youngshrijay@gmail.com>
2021-01-22 22:00:04 -0500

    Second checkin

---
+++ app1.py
@@ -0,0 +1,11 @@
+# My first program 
+# 1/22/2021
+print("Good morning, Good after noon, Good evening")
+#input return str, convert to int by using int function
+time = int(input("Enter your time:"))
+if time < 12:
+    print("Good Morning")
+elif time == 12:
+    print("it is noon")
+elif time > 6:
+    print("Good Afternoon")
